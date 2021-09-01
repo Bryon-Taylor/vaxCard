875fd5b1cd5ff8812d889ed3d63f107f71f4ae6f
bryon <taylor.bryon@gmail.com>
2021-08-31 18:16:28 -0700

    get registration data to authenticate and insert into db

--- index.js
+++ index.js
@@ -4,67 +4,110 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const session = require("express-session");
+LocalStrategy = require('passport-local').Strategy;
 const app = express();
 const PORT = 3000;
 
 // use "public" folder to access resources and assets like css and image files
 app.use(express.static("public"));
 
+// Allows parsing of incoming HTTP request bodies in the "req" variable
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// session intial configuration
+app.use(session({
+  secret: "my secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// setup Passport for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connect (or create if doesn't exist) to database on default port 27017
-mongoose.connect("mongodb://localhost:27017/vaxCardUserDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/vaxCardUserDB", {
+  useNewUrlParser: true
+});
 // TODO: close database connection at some point
 
 // structure for documents inserted into the db
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Name is required"]
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"]
-  },
+  email: String,
+  password: String,
   imgUrl: String // URL that points to vax card image
 });
 
-// creates a User model to access the collection of "user" "s that follow the userSchema
+// to salt and hash our passwords and save users into database
+userSchema.plugin(passportLocalMongoose);
+
+// creates a User model to access the collection of "user"s that follow the userSchema
 const User = mongoose.model("User", userSchema);
 
-const newUser = new User({
-  username: "Bryon",
-  password: "password",
-  imgUrl: "https://www.vaxcard.com"
-});
+// use default local strategy that looks for html name fields "username", " password"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// const newUser = new User({
+//   username: "Bryon",
+//   password: "password",
+//   imgUrl: "https://www.vaxcard.com"
+// });
 // newUser.save();
 
-// Allows parsing of incoming HTTP request bodies in the "req" variable
-app.use(bodyParser.urlencoded({extended: true}));
-
-// app.use(session({
-//   secret: "my secret",
-//   resave: false,
-//   saveUninitialized: false
-// }));
-//
-// // setup Passport for authentication
-// app.use(passport.initialize());
-// app.use(passport.session());
-
 app.get("/", function(req, res) {
+  // if user is logged in
+  // if(req.isAuthenticated) {
+  //   res.redirect("/loggedIn");
+  // }
   // __dirname gets absolute filepath
   console.log("the dirname is: " + __dirname);
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-  console.log(req.body.password);
+app.get("/loggedIn", function(req, res) {
+  if (req.isAuthenticated) {
+    res.sendFile(__dirname + "/loggedIn.html");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/register", function(req, res) {
+  console.log(req.body.email + " " + req.body.password + " " + req.body.password2);
+
+  // register method comes from passport-local-mongoose package, don't have to
+  // manually create new User. Default looks for html name fields "username", "password"
+  User.register({username: req.body.username}, req.body.password, function(err, newUser) {
+    console.log("newUser: " + newUser);
+    if (err) {
+      console.log("the error is: " + err);
+      res.redirect("/");
+    } else {
+
+      // authenticate using local strategy, callback triggered on success
+      passport.authenticate("local")(req, res, function() {
+        console.log("authentication successful");
+        res.redirect("/loggedIn"); // goes to loggedIn route
+      });
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  console.log(req.body.username + " " + req.body.password);
+});
+
+app.post("/logout", function(req, res) {
+  console.log(req.body);
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(PORT, function() {
   console.log("Express started on port 3000");
 });
-
-function toggleLoginRegister() {
-  console.log("toggling");
-  alert("worked!");
-}
--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,41 @@
+// if on login card and "register" text is clicked, rotate to register card
 document.getElementById("registerText").addEventListener("click", function(event) {
   document.getElementById("card-container").setAttribute("style", "transform: rotateY(180deg)");
 });
 
+// if on Register card and "login" text is clicked, rotate to login card
 document.getElementById("loginText").addEventListener("click", function(event) {
   // alert("clicked");
   document.getElementById("card-container").setAttribute("style", "transform: rotateY(0deg)");
 });
-// document.getElementById("loginRegister").addEventListener("click", function(event) {
-//   console.log("clicked");
-//   event.preventDefault();
-// });
+
+// "input" monitors changes as user types
+document.getElementById("password1").addEventListener("input", function(e) {
+  checkPasswordEquality();
+});
+
+// "input" monitors changes as user types
+document.getElementById("password2").addEventListener("input", function(e) {
+  checkPasswordEquality();
+});
+
+function checkPasswordEquality() {
+  password1 = document.getElementById("password1").value;
+  password2 = document.getElementById("password2").value;
+  registerBtn = document.getElementById("registerBtn");
+  if(password1 !== password2) {
+      registerBtn.innerHTML = "passwords don't match";
+      registerBtn.style.backgroundColor = "#CF0000";
+
+      // disable the ability to submit form if password fields don't match
+      registerBtn.setAttribute("type", "button");
+      console.log(registerBtn.getAttribute("type"));
+  } else {
+    registerBtn.innerHTML = "Register";
+
+    // eneable the ability to submit a form when password fields match
+    registerBtn.setAttribute("type", "submit");
+    registerBtn.style.backgroundColor = "#548CA8";
+    console.log(registerBtn.getAttribute("type"));
+  }
+}
